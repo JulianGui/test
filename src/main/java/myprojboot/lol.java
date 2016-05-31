e1f875732d67a7fbb4c4f50dfbeab976f4bf0998
Julian <julian.guillet@gmail.com>
2016-05-31 11:37:13 +0200

    lolcommit

---
+++ src/main/java/myprojboot/lol.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package myprojboot;
+
+/**
+ * @author utilisateur
+ *
+ */
+public class lol {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
